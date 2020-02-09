6da9cde759859a78a9466e265e1fba1183cffd27
Berehulia <n.berehulia@gmail.com>
2020-02-09 17:06:44 +0200

    Test Droid realisation.

--- src/main/java/ua/training/droidwars/model/berehulia/TestDroid.java
+++ src/main/java/ua/training/droidwars/model/berehulia/TestDroid.java
@@ -4,29 +4,42 @@ import ua.training.droidwars.model.Droid;
 
 public class TestDroid extends Droid {
 
+    public TestDroid() {
+        this.attack = 5;
+        this.defence = 5;
+        this.health = 50;
+        this.name = "TestDroid";
+    }
+
     @Override
     public int useAttack(Droid droid) {
+        droid.setHealth(droid.getHealth() - this.attack);
         return 0;
     }
 
     @Override
     public int useDefence(Droid droid) {
+        this.health += 5;
         return 0;
     }
 
     @Override
     public int useSpecialAbility(Droid droid) {
+        this.setHealth(this.getHealth() + 20);
         return 0;
     }
 
     @Override
     public int useUltimateAbility(Droid droid) {
+        droid.setName("LoserDroid!");
         return 0;
     }
 
     @Override
     public void prepareForNextBattle() {
-
+        this.attack = 5;
+        this.defence = 5;
+        this.health = 50;
     }
 
 }
